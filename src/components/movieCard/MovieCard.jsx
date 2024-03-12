57b77e45eccf790d85169779024cf9fe15073711
theophilusnuer <tphilus003@gmail.com>
2024-03-12 14:54:53 +0000

    editing the moviedetails

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie, id }) => {
     return (
         <div className="flex-shrink-0 mr-4" style={{ width: "200px", height: "300px" }}>
             <div className="bg-gray-800 rounded-lg shadow-lg p-2 flex flex-col justify-between h-full">
                 <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-full h-auto rounded-t-md object-cover" style={{ height: "70%" }} />
                 <div className="movie-info mt-2">
                     <h2 className="text-lg font-semibold text-gray-300">
-                        <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+                        <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
                     </h2>
                     <p className="text-sm text-gray-400">{movie.release_date}</p>
                     {/* <p className="text-sm text-gray-300 truncate">{movie.overview}</p> */}
--- src/pages/moviedetails/index.jsx
+++ src/pages/moviedetails/index.jsx
@@ -1,34 +1,44 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-
 export default function MovieDetails() {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
-    
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_MOVIE_API_KEY}
-`)
-            .then(response => response.json())
-            .then(data => {
-                setMovie(data.results);
-            })
-            .catch(error => {
-                console.error('Error fetching movie details:', error)
-            });
+        const fetchMovieDetails = async () => {
+            try {
+                const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch movie details');
+                }
+                const data = await response.json();
+                setMovie(data); // Update state with fetched movie details
+            } catch (error) {
+                console.error('Error fetching movie details:', error);
+            }
+        };
 
+        fetchMovieDetails();
     }, [id]);
 
     if (!movie) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
 
     return (
-        <div>
-            <h1> {movie.title} </h1>
-            <p> {movie.tagline} </p>
-            <p> {movie.overview} </p>
-        </div>
+        <section>
+            <div className="relative w-screen h-screen">
+                <img className='absolute inset-0 w-full h-full object-cover' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                <div class="absolute inset-0 bg-black opacity-70"></div>
+                <img className='h-20 rounded-md' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+            </div>
+            <h1>{movie.title}</h1>
+            <p>{movie.tagline}</p>
+            <p>{movie.overview}</p>
+            <p className="">ratings</p>
+            release date
+            reviews
+        </section>
     );
 }
